6516f1f83a1606f158c9c87763f6564da0980f11
Shadow0X90 <justinMX90@gmail.com>
2014-04-01 09:08:07 -0400

    Added Config file

--- MultiMassive/src/com/ps2j/MultiMassive/MultiMassiveMain.java
+++ MultiMassive/src/com/ps2j/MultiMassive/MultiMassiveMain.java
@@ -3,11 +3,66 @@ package com.ps2j.MultiMassive;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import com.ps2j.MultiMassive.jobs.JobPickListener;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 
 public class MultiMassiveMain extends JavaPlugin{
 
+File configFile;
+FileConfiguration config;
+
+
 public void onEnable(){
+	configFile = new File(getDataFolder(), "config.yml");
 	getServer().getPluginManager().registerEvents(new JobPickListener(), this);
+try {
+	firstRun();
+} catch (Exception e) {
+	e.printStackTrace();
+}
+	config = new YamlConfiguration();
+	loadYamls();
+}
+private void firstRun() throws Exception {
+	if(!configFile.exists()) {
+		configFile.getParentFile().mkdirs();
+		copy(getResource("config.yml"), configFile);
+	}
+}
+private void copy(InputStream in, File file) {
+	try {
+		OutputStream out = new FileOutputStream(file);
+		byte[] buf = new byte[1024];
+		int len;
+		while((len=in.read(buf))>0 {
+			out.write(buf,0,len);
+		}
+		out.close();
+		in.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+}
+public void saveYamls() {
+	try {
+		config.save(configFile);
+	} catch (IOException e) {
+		e.printStackTrace();
+	}
+	}
+public void loadYamls() {
+	try {
+		config.load(configFile);
+	} catch (Exception e) {
+		e.printStackTrace()
+	}
+}
+}
 }
 	
 }
