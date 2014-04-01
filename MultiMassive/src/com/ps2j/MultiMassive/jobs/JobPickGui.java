49555cf5d288c75466df9874678ca96682c58127
Shadow0X90 <justinMX90@gmail.com>
2014-04-01 08:15:27 -0400

    Added 2 jobs
    Dark Knight
    Warrior

--- MultiMassive/src/com/ps2j/MultiMassive/jobs/JobPickGui.java
+++ MultiMassive/src/com/ps2j/MultiMassive/jobs/JobPickGui.java
@@ -12,12 +12,12 @@ import org.bukkit.inventory.meta.ItemMeta;
 
 public class JobPickGui implements Listener{
 public static Inventory jobSelect = Bukkit.createInventory(null, 9, ChatColor.BLUE + "Select Job");
-public static void createDisplay(Material material, Inventory inv, int Slot, String name, String lore) {
+public static void createDisplay(Material material, Inventory inv, int Slot, String name, String lore1, String lore2) {
 ItemStack item = new ItemStack(material);
 ItemMeta meta = item.getItemMeta();
 meta.setDisplayName(name);
 ArrayList<String> Lore = new ArrayList<String>();
-Lore.add(lore);
+Lore.add(lore1, lore2);
 meta.setLore(Lore);
 item.setItemMeta(meta);
  
@@ -25,7 +25,8 @@ inv.setItem(Slot, item);
  
 }
 static {
-JobPickGui.createDisplay(Material.GOLD_SWORD, jobSelect, 1, "Paladin", "Healer tank.");
+JobPickGui.createDisplay(Material.ENDER_PEARL, jobSelect, 8, ChatColor.DARK_GRAY + "Dark Knight", ChatColor.DARK_GRAY + "A heavy offensive job that uses health to attack.", ChatColor.GOLD + "Wields Axes and Swords.");
+JobPickGui.createDisplay(Material.IRON_SWORD, jobSelect, 0, ChatColor.YELLOW + "Warrior", ChatColor.YELLOW + "A heavy offensive job with a low defence.", ChatColor.GOLD + "Wields Axes and Swords.");
 }
  
 }
