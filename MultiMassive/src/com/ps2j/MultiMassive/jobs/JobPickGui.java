827bb2406f1622920c1b827d718c940c2183b696
Shadow0X90 <JustinMX90@gmail.com>
2014-03-31 20:29:13 -0400

    Menu system redone is now functional.

--- MultiMassive/src/com/ps2j/MultiMassive/jobs/JobPickGui.java
+++ MultiMassive/src/com/ps2j/MultiMassive/jobs/JobPickGui.java
@@ -1,27 +1,31 @@
 package com.ps2j.MultiMassive.jobs;
 
+import java.util.ArrayList;
+
 import org.bukkit.Bukkit;
-import org.bukkit.Color;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.entity.Player;
-import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.Listener;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
-public class JobPickGui{
-public static Inventory jobSelect = Bukkit.createInventory(null, 1, Color.BLUE + "Select Job");
+public class JobPickGui implements Listener{
+public static Inventory jobSelect = Bukkit.createInventory(null, 9, ChatColor.BLUE + "Select Job");
+public static void createDisplay(Material material, Inventory inv, int Slot, String name, String lore) {
+ItemStack item = new ItemStack(material);
+ItemMeta meta = item.getItemMeta();
+meta.setDisplayName(name);
+ArrayList<String> Lore = new ArrayList<String>();
+Lore.add(lore);
+meta.setLore(Lore);
+item.setItemMeta(meta);
+ 
+inv.setItem(Slot, item);
+ 
+}
 static {
-jobSelect.setItem(0, new ItemStack(Material.GOLD_SWORD, 1));
+JobPickGui.createDisplay(Material.GOLD_SWORD, jobSelect, 1, "Paladin", "Healer tank.");
 }
-public void onInventoryClick(InventoryClickEvent event) {
-	Player player = (Player) event.getWhoClicked();
-	ItemStack clicked = event.getCurrentItem();
-	Inventory inventory = event.getInventory();
-	if (inventory.getName().equals(jobSelect.getName())) {
-		if (clicked.getType() == Material.GOLD_SWORD) {
-			event.setCancelled(true);
-            player.sendMessage("Test");
-		}
-	}
-}
+ 
 }
--- MultiMassive/src/com/ps2j/MultiMassive/jobs/JobPickListener.java
+++ MultiMassive/src/com/ps2j/MultiMassive/jobs/JobPickListener.java
@@ -1,18 +1,42 @@
 package com.ps2j.MultiMassive.jobs;
 
-import org.bukkit.Color;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+
+
+import com.ps2j.MultiMassive.MultiMassiveMain;
+import com.ps2j.MultiMassive.jobs.JobPickGui;
 
 public class JobPickListener implements Listener{
+    public JobPickListener(MultiMassiveMain plugin) {
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+    }
 	@EventHandler
-	public void onPlayerJoinEvent(PlayerJoinEvent event) {
-	  if(event.getPlayer().hasPlayedBefore()) {	 
-		Player player = event.getPlayer();
-	    player.sendMessage(Color.RED + "[MultiMassive] " + Color.WHITE + "Please select a Job");
+	public void onJobItemRun(PlayerInteractEvent event) {
+		final Player player = event.getPlayer();
+		if(player.getItemInHand().getType() == Material.DIRT) {
+	    player.sendMessage(ChatColor.RED + "[MultiMassive] " + ChatColor.WHITE + "Please select a Job");
 	    player.openInventory(JobPickGui.jobSelect);
-	  }
+		}
+		}
+	@EventHandler
+	public void onInventoryClick(InventoryClickEvent event) {
+		Player player = (Player) event.getWhoClicked();
+		ItemStack clicked = event.getCurrentItem();
+		Inventory inventory = event.getInventory();
+		if (inventory.getName().equals(JobPickGui.jobSelect.getName())) {
+			if (clicked.getType() == Material.GOLD_SWORD) {
+				player.closeInventory();
+	            player.sendMessage("Test");
+			}
+		}
 	}
 }
